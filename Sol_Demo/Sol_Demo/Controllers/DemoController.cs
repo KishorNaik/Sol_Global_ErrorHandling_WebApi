58c9695411465f111a40c6a533b8af7c245130ff
KishorNaik <kishor.naik011.net@gmail.com>
2020-03-18 13:26:45 +0530

    Code Commit

---
+++ Sol_Demo/Sol_Demo/Controllers/DemoController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Sol_Demo.Models;
+
+namespace Sol_Demo.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/demo")]
+    [ApiController]
+    public class DemoController : ControllerBase
+    {
+        [HttpPost("badrequest")]
+        public IActionResult BadRequestDemo()
+        {
+            return base.BadRequest(new ErrorModel()
+            {
+                StatusCode = 400,
+                Message = "Bad Request"
+            });
+        }
+
+        [HttpPost("onexception")]
+        public IActionResult OnExceptionDemo()
+        {
+            throw new Exception("Exception");
+        }
+    }
+}
---
+++ Sol_Demo/Sol_Demo/Models/ErrorModel.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sol_Demo.Models
+{
+    public class ErrorModel
+    {
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
